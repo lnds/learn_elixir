4319b912a92f8f971735efe26e7c1e254f1b6467
Eduardo Díaz <ediaz@dcc8090.cl>
2021-03-14 20:36:44 -0300

    misc exercises

---
+++ mm/default_params.exs
@@ -0,0 +1,6 @@
+defmodule Example do
+    def func(p1, p2 \\ 2, p3 \\ 3, p4) do
+        IO.inspect [p1, p2, p3, p4]
+    end
+
+end
---
+++ mm/exercises.exs
@@ -0,0 +1,11 @@
+defmodule Fun do
+
+    def sum(1), do: 1
+
+    def sum(n), do: n + sum(n - 1)
+
+
+    def gcd(x, 0), do: x
+
+    def gcd(x, y), do: gcd(y, rem(x, y))
+end
