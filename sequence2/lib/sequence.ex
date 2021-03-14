694548883b587200d4fde92a95290e44164501a3
Eduardo Díaz <ediaz@dcc8090.cl>
2021-03-14 20:37:07 -0300

    sequence from the book

---
+++ sequence2/lib/sequence.ex
@@ -0,0 +1,18 @@
+defmodule Sequence do
+  @moduledoc """
+  Documentation for `Sequence`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sequence2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
